0ab6e1e9b37fbeb933d415a87c6af103b11b4349
Luca Broggiato <luca.broggiato@edu.unito.it>
2022-01-13 15:51:07 +0100

    Edit to the program, in order to use Observer-Observable pattern instead of classy method calls.

--- src/Exercise1.java
+++ src/Exercise1.java
@@ -1,17 +1,21 @@
 import java.util.*;
 
 public class Exercise1 {
-    
+
     public static void main(String[] args) {
 
         Visualizer v = new Visualizer();
         Filter f = new Filter(v);
         Counter c = new Counter(f);
+
+        c.addObserver(f);
+        f.addObserver(v);
+
         c.start();
     }
 }
 
-class Counter{
+class Counter extends Observable{
 
     private int c;
     private Filter filter;
@@ -26,13 +30,14 @@ class Counter{
         for (int i=0; i<50; i++) {
             c++;
             if (c%5==0) {
-                filter.filter(c);
+                setChanged();
+                notifyObservers(c);
             }
         }
     }
 }
 
-class Filter {
+class Filter extends Observable implements Observer {
 
     private List<Integer> list;
     Visualizer visualizer;
@@ -42,24 +47,27 @@ class Filter {
         list = new ArrayList<Integer>();
     }
 
-    public void filter(int c) {
-        list.add(c);
-        if (list.size()%2==0) {
-
-            System.out.println("lista size: " + list.size());
-
-            visualizer.visualize(list);
+    @Override
+    public void update(Observable o, Object arg) {
+        if (o != null && o instanceof Counter){
+            list.add((Integer) arg);
+            if (list.size()%2==0) {
+                setChanged();
+                notifyObservers(list);
+            }
         }
     }
 }
 
-class Visualizer {
+class Visualizer implements Observer {
 
-    public void visualize(List<Integer> list) {
-
-        for (Integer i : list) {
-            System.out.println(i.intValue());
+    @Override
+    public void update(Observable o, Object arg) {
+        if (o != null && o instanceof Filter) {
+            for (Integer i : (List<Integer>) arg) {
+                System.out.println(i.intValue());
+            }
+            System.out.println();
         }
-        System.out.println();
     }
 }
